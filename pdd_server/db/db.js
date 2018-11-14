334bd0887dcba669439a8216af58901d1a6607b9
UriChen <urichen9606@gmail.com>
2018-11-14 10:07:32 +0800

    修复mysql连接丢失问题

--- pdd_server/db/db.js
+++ pdd_server/db/db.js
@@ -1,29 +1,90 @@
 // 引入mysql.js
+// const mysql = require('mysql');
+// // import mysql from 'mysql' 也可以
+// // // 配置mysql
+// // const conn = mysql.createConnection({
+// //   host: '127.0.0.1', // 数据库地址
+// //   user: 'root', // 账号
+// //   password: '123456', // 密码
+// //   database: 'pdd_shop' // 数据库名称
+// // });
+// // // 连接数据库
+// // conn.connect();
+// // module.exports = conn;
+
+// MySQL数据库联接配置
+const  mysql_config = {
+  host: '127.0.0.1',
+  user: 'root',
+  password: '123456',
+  database:'pdd_shop', //数据库
+};
+//单个连接的多语句查询配置
+const  mysql_config_multi = {
+  host: mysql_config.host,
+  user: mysql_config.user,
+  password: mysql_config.password,
+  database:mysql_config.database, //数据库
+  port: mysql_config.port,
+  multipleStatements:true
+};
+// 实现与MySQL交互
 const mysql = require('mysql');
-// import mysql from 'mysql' 也可以
-// 配置mysql
-const pool = mysql.createConnection({
-  host: '127.0.0.1', // 数据库地址
-  user: 'root', // 账号
-  password: '123456', // 密码
-  database: 'pdd_shop' // 数据库名称
-});
+// 使用连接池，提升性能
+const poolConn = mysql.createPool(mysql_config);
 
-const query = function(sql,options,callback){
 
-  pool.getConnection(function(err,conn){
+//连接池查询
+const querypoll = function(sql,params,callback){
+  poolConn.getConnection(function(err,conn){
     if(err){
       callback(err,null,null);
     }else{
-      conn.query(sql,options,function(err,results,fields){
+      const query = conn.query(sql,params,function(qerr,vals,fields){
+        //释放连接
+        conn.release();
         //事件驱动回调
-        callback(err,results,fields);
+        callback(qerr,vals,fields);
       });
-      //释放连接，需要注意的是连接释放需要在此处释放，而不是在查询回调里面释放
-      conn.release();
+      console.log("sql:::"+query.sql);
     }
   });
 };
 
 
-module.exports = query;
+//不使用连接池
+const query = function (sql,params,callback) {
+  const conn = mysql.createConnection(mysql_config_multi);
+  //连接错误，2秒重试
+  conn.connect(function (err) {
+    if (err) {
+      console.log("error when connecting to db:", err);
+      setTimeout(query , 2000);
+    }else{
+      const query = conn.query(sql,params,function(qerr,vals,fields){
+        //关闭连接
+        conn.end();
+        //事件驱动回调
+        callback(qerr,vals,fields);
+      });
+      console.log("sql:::"+query.sql);
+    }
+  });
+
+
+  conn.on("error",function (err) {
+    console.log("db error", err);
+    // 如果是连接断开，自动重新连接
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      query();
+    } else {
+      throw err;
+    }
+  });
+};
+
+
+module.exports = {
+  querypoll:querypoll,
+  query:query
+};
