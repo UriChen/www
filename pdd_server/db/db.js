dcbc8b9a802f2d6083951d25c43c7864fee568e0
UriChen <urichen9606@gmail.com>
2018-11-14 09:43:01 +0800

    .

--- pdd_server/db/db.js
+++ pdd_server/db/db.js
@@ -2,12 +2,24 @@
 const mysql = require('mysql');
 // import mysql from 'mysql' 也可以
 // 配置mysql
-const conn = mysql.createConnection({
+const mysqlConfig = mysql.createConnection({
   host: '127.0.0.1', // 数据库地址
   user: 'root', // 账号
   password: '123456', // 密码
   database: 'pdd_shop' // 数据库名称
 });
-// 连接数据库
-conn.connect();
+
+const conn = function(sql,callback){
+  mysqlConfig.getConnection(function(err,conn){
+    if(err){
+      callback(err,null);
+    }else{
+      conn.query(sql,function(err,results){
+        callback(err,results);
+      });
+      conn.release();
+    }
+  });
+};
+
 module.exports = conn;
